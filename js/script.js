50e793f6d824b6febe57923d726bc8357349196e
Marco Fiordi <marco.fiordi97@gmail.com>
2026-03-26 15:42:44 +0100

    impostata struttura esercizio mail con pseudocodice e array email

---
+++ js/script.js
@@ -0,0 +1,25 @@
+'use strict'
+//1: creo una lista di email inviate
+//2: chiedo all'utente di inserire la sua email
+//3: creo una variabile che mi dice se ho trovato l'email all'inizio e la imposto  false
+//4: scorro tutte le email della lista con un ciclo for
+    // se l'email inserita dall'utente è uguale all'email corrente della lista imposto la variabile true
+
+// finito il ciclo
+// se la variabile è true
+    // stampa "accesso consentito"
+    
+// altrimenti
+    // stampo "accesso negato"
+
+const listaEmail = [
+    'marco@gmail.com', 
+    'giovanni@gmail.com', 
+    'giuseppe@gmail.com', 
+    'alfredo@gmail.com', 
+    'fernando@gmail.com', 
+    'ettore@gmail.com', 
+    'franco@gmail.com',
+    'mario@gmail.com',
+    'giancarlo@gmail.com',
+    'gianni@gmail.com' ];    
